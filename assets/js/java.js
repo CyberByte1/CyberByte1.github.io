25c463f9846367f82c5c8cd4a24132826480eda3
CyberByte1 <152000991+CyberByte1@users.noreply.github.com>
2023-11-25 22:48:12 +0100

    First online upload of local edit

---
+++ assets/js/java.js
@@ -0,0 +1,39 @@
+
+
+// smooth scroll
+document.getElementById("downloadButton1").addEventListener("click", function() {
+	
+	var downloadLink = document.createElement("a");
+	downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
+	downloadLink.download = "Moe_Kaadi_Resume.pdf";
+	document.body.appendChild(downloadLink);
+	downloadLink.click();
+	document.body.removeChild(downloadLink);
+})
+	document.getElementById("downloadButton2").addEventListener("click", function() {
+		
+		var downloadLink = document.createElement("a");
+		downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
+		downloadLink.download = "Moe_Kaadi_Resume.pdf";
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+		document.body.removeChild(downloadLink);
+	
+});
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
