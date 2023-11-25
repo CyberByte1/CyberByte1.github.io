d944b67e99a3821261c53c98b4e1f57b374e810c
CyberByte1 <152000991+CyberByte1@users.noreply.github.com>
2023-11-25 23:20:50 +0100

    Update java.js to fix PDF not downloading on IOS

--- assets/js/java.js
+++ assets/js/java.js
@@ -2,23 +2,28 @@
 
 // smooth scroll
 document.getElementById("downloadButton1").addEventListener("click", function() {
-	
-	var downloadLink = document.createElement("a");
-	downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
-	downloadLink.download = "Moe_Kaadi_Resume.pdf";
-	document.body.appendChild(downloadLink);
-	downloadLink.click();
-	document.body.removeChild(downloadLink);
-})
-	document.getElementById("downloadButton2").addEventListener("click", function() {
-		
-		var downloadLink = document.createElement("a");
-		downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
-		downloadLink.download = "Moe_Kaadi_Resume.pdf";
-		document.body.appendChild(downloadLink);
-		downloadLink.click();
-		document.body.removeChild(downloadLink);
-	
+    var downloadLink = document.createElement("a");
+    downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
+    downloadLink.target = "_blank"; // Open in a new tab/window
+    downloadLink.download = "Moe_Kaadi_Resume.pdf";
+
+    // Trigger a click event on the link
+    downloadLink.dispatchEvent(new MouseEvent('click'));
+
+    // Remove the link from the DOM after the click event
+    document.body.removeChild(downloadLink);
+});
+document.getElementById("downloadButton2").addEventListener("click", function() {
+    var downloadLink = document.createElement("a");
+    downloadLink.href = "/Moe_Kaadi_Resume.pdf"; // Provide the correct path to your CV file
+    downloadLink.target = "_blank"; // Open in a new tab/window
+    downloadLink.download = "Moe_Kaadi_Resume.pdf";
+
+    // Trigger a click event on the link
+    downloadLink.dispatchEvent(new MouseEvent('click'));
+
+    // Remove the link from the DOM after the click event
+    document.body.removeChild(downloadLink);
 });
 $(document).ready(function(){
 	$(".nav-link").on('click', function(event) {
